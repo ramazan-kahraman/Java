6fb965eb54c1e6a933c6a6d21908fb95529a7e34
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-22 14:30:27 +0300

    Add files via upload

---
+++ c02_Variables.java
@@ -0,0 +1,15 @@
+package ders01_programlamayagiriş;
+
+public class c02_Variables {
+
+    public static void main(String[] args) {
+
+        int sayi= 111;
+
+        System.out.println("Hello Java");
+        System.out.println(sayi);
+
+
+    }
+
+}
