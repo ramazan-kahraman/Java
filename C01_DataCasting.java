412bb2ee51f6afe04ab7e664ad244b2b56c2faf5
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-22 14:30:41 +0300

    Add files via upload

---
+++ C01_DataCasting.java
@@ -0,0 +1,31 @@
+package ders03_datacasting_Wrapperclass;
+
+import java.util.Scanner;
+
+public class C01_DataCasting {
+    public static void main(String[] args) {
+
+       // String str=20;
+      //  boolean mutluMu="true";
+       // int a=true;
+
+
+        double s=20;
+
+        int sayi=15;
+
+        long ln=sayi;
+
+        short sh=23;
+        int k=sh;
+
+        long lng=sh;
+        float fd=lng;
+
+
+
+
+
+
+    }
+}
