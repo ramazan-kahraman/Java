c407b8bfe5e02a7ac507d1b6dad2a79b04c534e5
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-22 14:30:11 +0300

    Add files via upload

---
+++ c01_helloworld.java
@@ -0,0 +1,22 @@
+package ders01_programlamayagiriş;
+
+public class c01_helloworld {
+
+    public static void main(String[] args) {
+
+        // IT dunyasına merhaba diyelim
+
+
+        System.out.println("Hello World");
+
+        /*
+        birden fazla
+        satırı
+        yorum haline
+        getirmek için bu yöntem kullanılır.
+         */
+
+
+    }
+
+}
