17800e8f94fa48da7b9f2df250e117d33a77f455
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-23 17:05:28 +0300

    Add files via upload

---
+++ C01_DataCasting.java
@@ -0,0 +1,15 @@
+package ders04_matematikselişlemler_incrementDecrement;
+
+public class C01_DataCasting {
+    public static void main(String[] args) {
+
+        int sayi1=20;
+        int sayi2=6;
+        System.out.println(sayi1/sayi2);//3,6666 çıkması lazım ama java iki sayı da int olduğundan sadece 3 yazdırır.
+
+        double sayi3=6;
+        System.out.println(sayi1/sayi3);//3,33333 olarak yazar çünkü java geniş olana göre çalışır ağbiii:)
+
+
+    }
+}
