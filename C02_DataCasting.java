43079eb9e273f2de3d97182003a21d0686641ea8
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-23 17:05:43 +0300

    Add files via upload

---
+++ C02_DataCasting.java
@@ -0,0 +1,17 @@
+package ders04_matematikselişlemler_incrementDecrement;
+
+import java.util.Scanner;
+
+public class C02_DataCasting {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("lutfen iki sayı girin");
+        double sayi1=scan.nextDouble();
+        double sayi2=scan.nextDouble();
+
+        int bölmeSonucuTamsayi=(int)(sayi1/sayi2);
+        System.out.println(bölmeSonucuTamsayi);
+
+    }
+}
