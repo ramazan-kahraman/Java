a74b497c89d9aeafde4d022defe4a91aa1d096db
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-24 12:16:12 +0300

    Add files via upload

---
+++ C06_Pre_Post_Increment.java
@@ -0,0 +1,24 @@
+package ders04_matematikselişlemler_incrementDecrement;
+
+public class C06_Pre_Post_Increment {
+    public static void main(String[] args) {
+
+        int a=10;
+
+        System.out.println(a);
+        a++;
+
+        int b=10;
+        b++;
+        System.out.println(b);
+
+        int c=10;
+        System.out.println("c : " +c++);
+
+        int d=10;
+        System.out.println("d : "+ ++d);
+
+
+
+    }
+}
