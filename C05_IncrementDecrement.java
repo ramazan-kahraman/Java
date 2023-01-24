c7992f8d0abb8b84a7eaf2a607985954d9a2e67f
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-24 12:15:40 +0300

    Add files via upload

---
+++ C05_IncrementDecrement.java
@@ -0,0 +1,28 @@
+package ders04_matematikselişlemler_incrementDecrement;
+
+public class C05_IncrementDecrement {
+    public static void main(String[] args) {
+
+        int sayi=10;
+        sayi=sayi+3;
+        sayi +=3;
+
+
+        int s=10;
+        s=s+1;
+        s+=1;
+        s++;
+        ++s;
+
+        int t=10;
+        t=t-1;
+        t-=1;
+        t--;
+        --t;
+
+
+
+
+
+    }
+}
