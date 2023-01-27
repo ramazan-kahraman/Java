72794e2978986556670b4177e1536ca283fd4a55
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-27 12:04:41 +0300

    Add files via upload

---
+++ C02_IfStatements.java
@@ -0,0 +1,20 @@
+package ders06_ıfElseStatements;
+
+import java.util.Scanner;
+
+public class C02_IfStatements {
+    public static void main(String[] args) {
+
+       Scanner scan=new Scanner(System.in);
+        System.out.println("lutfen ucgenin 3 kenar uzunlugunu yazin");
+
+        int kenar1= scan.nextInt();
+        int kenar2=scan.nextInt();
+        int kenar3= scan.nextInt();
+
+        if (kenar1==kenar2 || kenar2==kenar3){
+            System.out.println("eskenar ucgen");
+        }
+
+    }
+}
