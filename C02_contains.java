58507b51c449ef90f7438dcba75f12e85c8c2ca6
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-29 16:50:16 +0300

    Add files via upload

---
+++ C02_contains.java
@@ -0,0 +1,32 @@
+package ders10_stringManipulations;
+
+public class C02_contains {
+    public static void main(String[] args) {
+
+        String str="Java ile kodlama çok zevkli";
+
+        System.out.println(str.contains("çok"));    //true
+
+        System.out.println(str.contains("odla"));   //true
+
+        System.out.println(str.contains("a"));  //true
+
+        //System.out.println(str.contains('s'));  //parametre olarak char kabul etmez
+
+        //CharSquence : char dizisi
+
+        System.out.println(str.contains("java"));   //false
+
+        String str2="Java";
+        System.out.println(str.contains(str2)); //true
+
+
+
+
+
+
+
+
+
+    }
+}
