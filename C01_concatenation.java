a8c9bc0c5bed5e2258072dcc755966d924a5be5f
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-29 16:50:05 +0300

    Add files via upload

---
+++ C01_concatenation.java
@@ -0,0 +1,29 @@
+package ders10_stringManipulations;
+
+public class C01_concatenation {
+    public static void main(String[] args) {
+
+        String str1="Java";
+        String str2="Guzeldir";
+        boolean bl=true;
+        int sayi=20;
+        double dbl=3.54;
+
+        System.out.println(str1+bl+sayi);   //Javatrue20
+
+         //  System.out.println(bl+sayi);
+        //String dışındaki data türlerinde toplama yapmamıza izin vermeyebilir.
+
+        System.out.println(sayi+dbl);   //23,54 geniş olana göre yapar.
+
+         //System.out.println(str1.concat(bl));
+        //concat() sadece String variable ları birleştirmek için kullanılır.
+        System.out.println(str1.concat(" ").concat(str2));   //Java Güzeldir
+
+
+
+
+
+
+    }
+}
