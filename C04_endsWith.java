39a726378aeee98d893926eddc819be5d2025f08
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-01-30 12:32:17 +0300

    Add files via upload

---
+++ C04_endsWith.java
@@ -0,0 +1,24 @@
+package ders10_stringManipulations;
+
+public class C04_endsWith {
+    public static void main(String[] args) {
+
+        String str="Java heryerde guzel";
+
+        System.out.println(str.endsWith("l"));  //true
+
+        System.out.println(str.endsWith(""));   //true
+
+        System.out.println(str.endsWith("guzel"));  //true
+
+
+
+
+
+
+
+
+
+
+    }
+}
