0e86f24d998eddd9b04cf17aa68682cd69240908
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-02-10 17:21:15 +0300

    Add files via upload

---
+++ C09_MethodKullanma.java
@@ -0,0 +1,14 @@
+package ders13_nestedForLoop_methodOlusturma;
+
+import ders06_ıfElseStatements.C08_IfElseIfStatements;
+
+public class C09_MethodKullanma {
+    public static void main(String[] args) {
+
+
+        C08_MethodOlusturma.isteToplaYazdir();
+
+
+
+    }
+}
