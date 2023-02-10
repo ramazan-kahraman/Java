ad0c280d47f6fc90b7aa29b8f1f874a9b57c492c
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-02-10 17:21:35 +0300

    Add files via upload

---
+++ C08_MethodOlusturma.java
@@ -0,0 +1,28 @@
+package ders13_nestedForLoop_methodOlusturma;
+
+import java.util.Scanner;
+
+public class C08_MethodOlusturma {
+    public static void main(String[] args) {
+
+        /*
+        kullanıcıdan iki sayi alip, bunlarin taoplamini yazdiran bir method olusturun.
+        */
+
+        isteToplaYazdir();
+
+
+    }
+    public static void isteToplaYazdir(){
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("lutfen iki sayi girin");
+
+        double sayi1= scan.nextDouble();
+        double sayi2= scan.nextDouble();
+
+        System.out.println("girilen sayilarin toplami: "+(sayi1+sayi2));
+    }
+
+
+}
