d24708c43efd74114aed7dec5de38d3569799879
rkah1381 <119287759+rkah1381@users.noreply.github.com>
2023-02-12 17:05:40 +0300

    Add files via upload

---
+++ C02_MDA_ElementleriToplama.java
@@ -0,0 +1,32 @@
+package ders20_multiDimensionalArrays;
+
+public class C02_MDA_ElementleriToplama {
+    public static void main(String[] args) {
+
+        int[] arr1={3,5,7,8};   //23
+
+        int toplam=0;
+
+        for (int i = 0; i < arr1.length ; i++) {
+            toplam+=arr1[i];
+        }
+        System.out.println("Tek katli array elementler toplami: "+toplam);  //23
+
+        int [][]arr={{1,2},{3,4,5},{7}};
+        //elementlerini toplayalim
+
+        toplam=0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 0; j <arr[i].length ; j++) {
+
+                toplam+=arr[i][j];
+
+            }
+
+        }
+        System.out.println("İki katli arrayin toplami="+toplam);    //22
+
+    }
+}
